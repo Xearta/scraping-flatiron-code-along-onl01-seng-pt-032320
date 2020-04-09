ffb1b9821cd82b0e22ae8a62a5551870ea3f7746
Tanner Townsend <rsmcwow@gmail.com>
2020-04-09 03:26:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,7 +11,11 @@ class Scraper
   end
 
   def get_courses
-    self.get_page.css(".post").each do |post|
+    self.get_page.css(".post")
+  end
+
+  def make_courses
+    self.get_courses.each do |post|
       course = Course.new
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
